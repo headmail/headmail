afb0d6bd198408e6c9926a4fdd794f49c21fbbd8
Joseph Lee <joseph@jc-lab.net>
2025-07-17 15:08:30 +0000

    update

---
+++ cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Starting Headmail server...")
+	// TODO: Implement server initialization and startup
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/headmail/headmail
+
+go 1.24.5
